afee1dd860e40c87d8b9559b0bf8b5b98d65765d
jeremy-ml <jeremy.proton@gmail.com>
2015-01-16 20:23:12 +0000

    4 images

--- images.py
+++ images.py
@@ -9,8 +9,16 @@ from sklearn import datasets
 # Digits
 
 digits = datasets.load_digits()
+iris = datasets.load_iris()
 
 # Draw
 
-plt.imshow(digits.images[0])
+images_and_labels = list(zip(digits.images, digits.target))
+
+for index, (image, label) in enumerate(images_and_labels[:4]):
+    plt.subplot(2, 4, index + 1)
+    plt.axis('off')
+    plt.imshow(image)
+    plt.title('Training: %i' % label)
+
 plt.show()
