e1737153571b81eab5c0a790c0f6f7c5d1fee4ba
jeremy-ml <jeremy.proton@gmail.com>
2015-01-17 04:46:40 +0000

    Commit all

--- images.py
+++ images.py
@@ -12,11 +12,14 @@ digits = datasets.load_digits()
 iris = datasets.load_iris()
 
 # Draw
+# Iterate over two list
 
 images_and_labels = list(zip(digits.images, digits.target))
 
-for index, (image, label) in enumerate(images_and_labels[:4]):
-    plt.subplot(2, 4, index + 1)
+plt.figure(figsize=(6*3.13, 4*3.13))
+
+for index, (image, label) in enumerate(images_and_labels[:10]):
+    plt.subplot(2, 10, index + 1)
     plt.axis('off')
     plt.imshow(image)
     plt.title('Training: %i' % label)
